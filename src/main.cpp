6daedb4f0ddf7ecb5d1b83aefc5d7d6570bc7b82
DuckSupremacist <theo11sarda@gmail.com>
2025-04-07 01:45:00 +0200

    FIXED THE CODE AND FIXED THE FREAKING TESTS CASES!!!!

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,183 @@
+#include <array>
 #include <iostream>
+#include <span>
 #include <string>
+#include <vector>
 
-uint32_t getInitialHash() {
-    uint32_t hash = 0;
-    for (int i = 1e8; i > 0; i /= 10) {
-        int value;
-        std::cin >> value;
-        std::cin.ignore();
-        hash += value * i;
+using tile_index_t = long;
+constexpr int GRID_SIDE_SIZE = 3;
+constexpr int GRID_SIZE = GRID_SIDE_SIZE * GRID_SIDE_SIZE;
+constexpr uint32_t HASH_MODULO = 1 << 30;
+
+using tile_t = uint8_t;
+typedef std::array<tile_t, GRID_SIZE> Grid;
+
+class Position
+{
+  private:
+    size_t _distance_from_depth;
+    Grid _grid;
+    uint32_t _hash;
+
+    bool sumCandidates(const std::vector<tile_index_t>::iterator first, const std::vector<tile_index_t>::iterator end, const tile_index_t target_tile_index, const Grid& grid, std::vector<Position>& next_positions, tile_t sum = 0)
+    {
+        bool found = false;
+        for (auto it = first; it != end; ++it)
+        {
+            tile_index_t tile_index = *it;
+            tile_t candidate_sum = sum + _grid[tile_index];
+
+            if (candidate_sum > 6)
+            {
+                continue;
+            }
+
+            Grid next_grid = grid;
+            next_grid[target_tile_index] = candidate_sum;
+            next_grid[tile_index] = 0;
+            if (sum != 0)
+            {
+                found = true;
+                next_positions.emplace_back(_distance_from_depth - 1, next_grid);
+            }
+
+            if (sumCandidates(it + 1, end, target_tile_index, next_grid, next_positions, candidate_sum))
+            {
+                found = true;
+            }
+        }
+        return found;
     }
-    return hash;
-}
+
+    std::vector<Position> nextPositions()
+    {
+        // If the distance from depth is 0, return an empty vector
+        if (_distance_from_depth == 0)
+        {
+            return {};
+        }
+        std::vector<Position> next_positions;
+        for (tile_index_t i = 0; i < GRID_SIZE; ++i)
+        {
+            tile_t& tile = _grid[i];
+            if (tile != 0)
+            {
+                continue;
+            }
+
+            std::vector<tile_index_t> candidate_tiles;
+
+            // Left
+            if (i % GRID_SIDE_SIZE > 0)
+            {
+                tile_t& candidate_tile = _grid[i - 1];
+                if (candidate_tile != 0 && candidate_tile != 6)
+                {
+                    candidate_tiles.push_back(i - 1);
+                }
+            }
+
+            // Right
+            if (i % GRID_SIDE_SIZE < 2)
+            {
+                tile_t& candidate_tile = _grid[i + 1];
+                if (candidate_tile != 0 && candidate_tile != 6)
+                {
+                    candidate_tiles.push_back(i + 1);
+                }
+            }
+
+            // Up
+            if (i >= GRID_SIDE_SIZE)
+            {
+                tile_t& candidate_tile = _grid[i - GRID_SIDE_SIZE];
+                if (candidate_tile != 0 && candidate_tile != 6)
+                {
+                    candidate_tiles.push_back(i - GRID_SIDE_SIZE);
+                }
+            }
+
+            // Down
+            if (i < GRID_SIZE - GRID_SIDE_SIZE)
+            {
+                tile_t& candidate_tile = _grid[i + GRID_SIDE_SIZE];
+                if (candidate_tile != 0 && candidate_tile != 6)
+                {
+                    candidate_tiles.push_back(i + GRID_SIDE_SIZE);
+                }
+            }
+
+            // Not enough candidates to sum
+            if (candidate_tiles.size() < 2 || !sumCandidates(candidate_tiles.begin(), candidate_tiles.end(), i, _grid, next_positions))
+            {
+                // If no candidates were found, create a new position with the current tile set to 1
+                Grid next_grid = _grid;
+                next_grid[i] = 1;
+                next_positions.emplace_back(_distance_from_depth - 1, next_grid);
+                continue;
+            }
+        }
+        return next_positions;
+    }
+
+    size_t calculateHash()
+    {
+        std::vector<Position> next_positions = nextPositions();
+        if (next_positions.empty())
+        {
+            _hash = 0;
+            for (const tile_t& tile : _grid)
+            {
+                _hash = _hash * 10 + tile;
+            }
+            _hash %= HASH_MODULO;
+            return 1;
+        }
+        _hash = 0;
+        for (const Position& position : next_positions)
+        {
+            _hash += position.getHash();
+            _hash %= HASH_MODULO;
+        }
+        return next_positions.size();
+    }
+
+  public:
+    Position(size_t distance_from_depth, Grid position) :
+        _distance_from_depth(distance_from_depth),
+        _grid(position),
+        _hash(0)
+    {
+        calculateHash();
+    }
+
+    explicit Position() : _distance_from_depth(0), _grid{0}, _hash(0)
+    {
+        std::cin >> _distance_from_depth;
+        std::cin.ignore();
+        std::cerr << "Depth: " << _distance_from_depth << std::endl;
+        for (tile_t& tile : _grid)
+        {
+            int value;
+
+            std::cin >> value;
+            tile = static_cast<tile_t>(value);
+            std::cin.ignore();
+        }
+        std::cerr << "Initial position: ";
+        for (const tile_t& tile : _grid)
+        {
+            std::cerr << static_cast<int>(tile);
+        }
+        std::cerr << std::endl;
+        std::cerr << calculateHash() << " end positions." << std::endl;
+    }
+
+    [[nodiscard]] uint32_t getHash() const
+    {
+        return _hash;
+    }
+};
 
 #ifdef BUILD_TESTS
 int testMain()
@@ -19,14 +186,8 @@ int testMain()
 int main()
 #endif
 {
-    int depth;
-    std::cin >> depth;
-    std::cin.ignore();
-    std::cerr << "Depth: " << depth << std::endl;
-    uint32_t initial_hash = getInitialHash();
-    std::cerr << "Initial hash: " << initial_hash << std::endl;
-
-    std::cout << 0 << std::endl;
+    Position initial_position{};
+    std::cout << initial_position.getHash() << std::endl;
 
     return 0;
 }
--- tests/src/coding_game.test.cpp
+++ tests/src/coding_game.test.cpp
@@ -9,7 +9,7 @@ int testMain(); // Forward declaration of the main function
 struct TestParams
 {
     size_t depth;
-    uint32_t init_hash;
+    uint32_t init_position;
     uint32_t expected_output;
 };
 
@@ -23,10 +23,10 @@ INSTANTIATE_TEST_SUITE_P(
     ::testing::Values(
         // depth, init_hash, expected_output
         // Custom test cases
-        TestParams{1, 000000000, 111111111},                                       // end states: 9
-        TestParams{1, 666666660, 666666661},                                       // end states: 1
-        TestParams{1, 666666101, 666666020},                                       // end states: 1
-        TestParams{1, 666656101, (666656020 + 666606061 + 666606160) % (1 << 30)}, // end states: 3
+        TestParams{1, 000000000, 111111111},  // end states: 9
+        TestParams{1, 666666660, 666666661},  // end states: 1
+        TestParams{1, 666666101, 666666020},  // end states: 1
+        TestParams{1, 666656101, (666656020 + 666606061 + 666606160) % (1 << 30)},  // end states: 3
         // Coding game test cases
         TestParams{20, 60222161, 322444322},  // Test 1;  end states: 2
         TestParams{20, 506450064, 951223336}, // Test 2;  end states: 6
@@ -35,10 +35,10 @@ INSTANTIATE_TEST_SUITE_P(
         TestParams{8, 606000615, 76092874},   // Test 5;  end states: 1484
         TestParams{24, 300362102, 661168294}, // Test 6;  end states: 418440394
         TestParams{36, 604202400, 350917228}, // Test 7;  end states: 1014562252076
-        TestParams{32, 54105, 999653138},     // Test 8;  end states: 104530503002231
-        TestParams{40, 4024134, 521112022},   // Test 9;  end states: 946763082877
-        TestParams{40, 54030030, 667094338},  // Test 10; end states: 559238314648167
-        TestParams{20, 51000401, 738691369},  // Test 11; end states: 4017226136890
+        TestParams{32, 000054105, 999653138},     // Test 8;  end states: 104530503002231
+        TestParams{40, 004024134, 521112022},   // Test 9;  end states: 946763082877
+        TestParams{40, 054030030, 667094338},  // Test 10; end states: 559238314648167
+        TestParams{20, 051000401, 738691369},  // Test 11; end states: 4017226136890
         TestParams{20, 100352100, 808014757}  // Test 12; end states: 950995003182
     )
 );
@@ -51,7 +51,7 @@ TEST_P(MainIntegrationTest, BasicCase) {
 
     // Process the hash to be passed to the program
     std::ostringstream hash_digits;
-    hash_digits << std::setw(9) << std::setfill('0') << params.init_hash;
+    hash_digits << std::setw(9) << std::setfill('0') << params.init_position;
     std::string hash_str = hash_digits.str(); // e.g. "060254928"
 
     std::stringstream input_stream;
